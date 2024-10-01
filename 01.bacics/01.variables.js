4078387fdc725977ac15c362a4c7a324ccbd8db7
Abhinav Bahadur Singh <144931517+abhinavbahadursingh@users.noreply.github.com>
2024-10-01 15:37:10 +0000

    some more

---
+++ 01.bacics/01.variables.js
@@ -0,0 +1,9 @@
+const accountID = 12345;
+let accountEmail = "let@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ghaziabad"
+
+accountEmail = "newLet@gmail.com"
+accountPassword = "121212"
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
