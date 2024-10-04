8946c2268150bb01a1bc1ebc61924a5abb315858
Abhinav Bahadur Singh <144931517+abhinavbahadursingh@users.noreply.github.com>
2024-10-04 15:13:08 +0000

    array basics

--- 02.basics/01.arrays.js
+++ 02.basics/01.arrays.js
@@ -1,2 +1,20 @@
 const myArr = [1,2,3,4,5,6,7]
 console.log(myArr.includes(6));
+
+
+//slice , splice
+
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)//manuplates the original array.
+console.log(myn2);
+
+console.log("C" , myArr);
+
+
