ef6e81e190de21b6a19983dcf6582c34281143a5
Abhinav Bahadur Singh <144931517+abhinavbahadursingh@users.noreply.github.com>
2024-11-18 14:30:26 +0000

    gettter setter

---
+++ classes/getter_setter.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(email , password){
+        this.email = email
+        this.password  = password
+    }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){    
+        this._password = value    
+    }
+    
+}
+
+
+const abhinav = new User("abhi@12" , "123")
+console.log(abhinav.password);
